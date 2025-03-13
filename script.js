63c931957b023d33d65ed261254909ef7ddc38a6
iRaccoon13 <136942618+iRaccoon13@users.noreply.github.com>
2025-03-13 16:14:33 -0600

    Removed ability to edit directly due to bug; added functionality for c and v to add/remove
    Signed-off-by: iRaccoon13 <136942618+iRaccoon13@users.noreply.github.com>

--- script.js
+++ script.js
@@ -37,13 +37,14 @@ document.addEventListener("keydown", (e) => {
   if (document.activeElement == document.querySelector("h1")) {
     return
   }
-  if (e.key == " " || e.key == "=") {
+  if (e.key == " " || e.key == "=" || e.key == "v") {
     counter.add();
   } else if (Number(e.key) == e.key && e.key != "0") {
     counter.add(Number(e.key));
   } else if (e.key == "0") {
     counter.count = 0;
-  } else if (e.key == "-") {
+  } else if (e.key == "-" || e.key == "c") {
     counter.add(-1);
-  }
+  } else {
+  console.log(e.key)}
 })
